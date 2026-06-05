e2c17754054eef684ff007684178671643cd2dcc
G-Cranberry <ganeev.gd987@gmail.com>
2026-06-05 13:36:20 +0530

    Add log in js

---
+++ login.js
@@ -0,0 +1,6 @@
+document.getElementById('passengerCard').addEventListener('click', function () {
+  window.location.href = 'passenger-home.html';
+});
+document.getElementById('crewCard').addEventListener('click', function () {
+  window.location.href = 'crew-home.html';
+});
