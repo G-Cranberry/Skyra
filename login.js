6a0ccc1da71a4659900d2ff0ff8a01d7ff120cef
G-Cranberry <ganeev.gd987@gmail.com>
2026-06-05 20:18:14 +0530

    Update navigation links for passenger and crew cards

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 document.getElementById('passengerCard').addEventListener('click', function () {
-  window.location.href = 'passenger-home.html';
+  window.location.href = 'passenger-login.html';
 });
 document.getElementById('crewCard').addEventListener('click', function () {
-  window.location.href = 'crew-home.html';
+  window.location.href = 'crew-login.html';
 });
